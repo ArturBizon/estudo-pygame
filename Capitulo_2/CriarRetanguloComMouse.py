a0512d62a17b96a9585d58b30a9d0f357fa58a6f
Artur Bizon <artur_bizon@hotmail.com>
2020-03-21 17:59:37 -0300

    Desenhar retangulo com o mouse

---
+++ Capitulo_2/CriarRetanguloComMouse.py
@@ -0,0 +1,46 @@
+import pygame
+from pygame.locals import *
+
+BLACK = (0, 0, 0)
+GRAY = (127, 127, 127)
+WHITE = (255, 255, 255)
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+
+start = (0, 0,)
+end = (0,0,)
+size = (0, 0,)
+drawing = False
+running = True
+background = GRAY
+retangColor = BLUE
+
+pygame.init()
+screen = pygame.display.set_mode((640, 240))
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False 
+        elif event.type == MOUSEBUTTONDOWN:
+            start = event.pos 
+            size = (0, 0,)
+            drawing = True
+        elif event.type == MOUSEBUTTONUP:
+            size = (end[0] - start[0], end[1] - start[1])
+            drawing = False
+        elif event.type == MOUSEMOTION and drawing:
+            end = event.pos
+            size = (end[0] - start[0], end[1] - start[1])
+    
+    screen.fill(background)
+    pygame.draw.rect(screen, retangColor, (start, size), 8)
+    pygame.display.update()
+
+pygame.quit()
