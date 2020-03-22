16a25a7678202234f40045f8c36d86b559d0ee13
Artur Bizon <artur_bizon@hotmail.com>
2020-03-22 11:25:13 -0300

    Mover um retângulo com as setas

---
+++ Capitulo_3/MovimentacaoDeRetangulo.py
@@ -0,0 +1,45 @@
+import pygame
+from pygame.rect import *
+from pygame.locals import *
+
+BLACK = (0, 0, 0)
+GRAY = (127, 127, 127)
+WHITE = (255, 255, 255)
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)
+MAGENTA = (255, 0, 255)
+
+SIZE = 500, 200
+running = True
+background = GRAY
+comandos = {K_LEFT:(-5,0), K_RIGHT:(5,0), K_UP:(0,-5), K_DOWN:(0,5)}
+
+
+width, height = SIZE
+pygame.init()
+screen = pygame.display.set_mode(SIZE)
+
+rectPai = Rect(50, 60, 200, 80)
+rectFilho = rectPai.copy()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False 
+        elif event.type == KEYDOWN:
+            if event.key in comandos:
+                movimento = comandos[event.key]
+                rectFilho.move_ip(movimento)
+    
+    screen.fill(GRAY)
+    pygame.draw.rect(screen, BLUE, rectPai, 5)
+    pygame.draw.rect(screen, GREEN, rectFilho, 5)
+
+    pygame.display.flip()
+
+pygame.quit()
